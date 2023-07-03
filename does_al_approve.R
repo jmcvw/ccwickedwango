559b9dc09b3a37b447d9a186247e5073fb6ede24
alasdairgm <96436243+alasdairgm@users.noreply.github.com>
2023-07-03 14:22:56 +0100

    Added function

--- does_al_approve.R
+++ does_al_approve.R
@@ -1,6 +1,6 @@
 #' does_al_approve
-#' 
-#' A function to add the words "Al approves this message" or 
+#'
+#' A function to add the words "Al approves this message" or
 #' "Al doesn't approve this message" to a character input.
 #'
 #' @return A character vector
@@ -11,14 +11,14 @@
 
 does_al_approve <- function(text){
   stopifnot("Input is not text" = is.character(text))
-  
+
   outcome <- sample(c(1,2), size = 1)
-  
-  if outcome == 1(
-    paste(c(text, " - Al approves this message.")
-  )
-  else(
-    paste(c(text, " - Al does not approve this message.")
-  )
-  
+
+  if (outcome == 1){
+    paste0(text, " - Al approves this message.")
+  }
+  else {
+    paste0(text, " - Al does not approve this message.")
+  }
+
 }
