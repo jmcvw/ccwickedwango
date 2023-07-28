3f2971d020bfd2c5ad391f2f88dc21eada5c951f
alasdairgm <alasdairmorgan1991@gmail.com>
2023-07-28 14:06:48 +0100

    updated function

---
+++ r/assign_usernames.R
@@ -0,0 +1,28 @@
+#' assign_usernames
+#'
+#' A function that takes a vector of names and assigns usernames
+#' to each for use in a server. Requires tidyverse.
+#'
+#' @return A dataframe of student names and usernames
+#' @export
+#'
+#' @param names_vector character vector of student names
+#'
+#' @examples
+#' assign_usernames(c("John", "Colin"))
+
+assign_usernames <- function(names_vector){
+  stopifnot("Input is not a vector" = is.vector(names_vector))
+
+  user_df <- dplyr::as_tibble(names_vector) %>%
+    dplyr::rename(name = value) %>%
+    dplyr::rowid_to_column() %>%
+    dplyr::mutate(username = stringr::str_c("user", rowid)) %>%
+    dplyr::select(-rowid)
+
+  clipr::write_clip(user_df)
+  return(user_df)
+
+}
+
+
